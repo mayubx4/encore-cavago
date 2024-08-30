466aa53b1e984987f9b6057a2cabec07e8b032fc
ayub khan <ayubx4@gmail.com>
2024-08-30 20:53:06 +0500

    fix

--- src/app/profile/personalInformation/containers/mobile/personalInformationMobile.tsx
+++ src/app/profile/personalInformation/containers/mobile/personalInformationMobile.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './_personalInformationMobile.scss';
-import ProfileForm from '@app/profile/components/mobile/profileForm /profileForm';
+// import ProfileForm from '@app/profile/components/mobile/profileForm /profileForm';
 import ProfileHeader from '@app/profile/components/mobile/profileHeader/profileHeader';
 
 export default function PersonalInformationMobile() {
   return (
     <div className="personalInformationContainer">
       <ProfileHeader title="Personal information" />
-      <ProfileForm />
+      {/* <ProfileForm /> */}
     </div>
   );
 }
