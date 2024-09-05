a4580873c0a10219d19a8c0a7c1934530be75b12
ayub khan <ayubx4@gmail.com>
2024-09-05 16:20:43 +0500

    api fix

--- src/app/shared/api/axios.ts
+++ src/app/shared/api/axios.ts
@@ -1,15 +1,15 @@
-import { getToken } from '@shared/api/utils';
-import axios from 'axios';
+import { getToken } from "@shared/api/utils";
+import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-// const API_BASE_URL = 'https://dev2.mycavago.com/api';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = "https://dev2.mycavago.com/api";
 
 const getHeaders = () => {
-  const token = getToken() || '';
+  const token = getToken() || "";
 
-  return ({
-    Authorization: token ? `Bearer ${token}` : '',
-  });
+  return {
+    Authorization: token ? `Bearer ${token}` : "",
+  };
 };
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -18,7 +18,7 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => ({ ...config, headers: getHeaders() }),
-  (error) => Promise.reject(error),
+  config => ({ ...config, headers: getHeaders() }),
+  error => Promise.reject(error)
 );
 export default axiosInstance;
